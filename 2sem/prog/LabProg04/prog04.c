996702a1772b29ebcc5671d27dc5791c8ec411bb
Arthur Viana <arthursvianaufc@gmail.com>
2019-09-12 11:33:13 -0300

    Added prog04.c

---
+++ 2sem/prog/LabProg04/prog04.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void q1() {
+}
+
+void q2() {
+}
+
+void q3() {
+}
+
+void q4() {
+}
+
+void q5() {
+}
+
+void q6() {
+}
+
+void q7() {
+}
+
+void q8() {
+}
+
+int main(void) {
+  int n;
+  printf("Digite o número da questão: ");
+  scanf("%d", &n);
+  
+  switch(n) {
+    case 1: q1(); break;
+    case 2: q2(); break;
+    case 3: q3(); break;
+    case 4: q4(); break;
+    case 5: q5(); break;
+    case 6: q6(); break;
+    case 7: q7(); break;
+    case 8: q8(); break;
+    default : break;
+  }
+  return 0;
+}
